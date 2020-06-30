2c46c3b469e9a2adaa1a40448c8dd45c9ef58ce1
safchug <safchug@gmail.com>
2020-06-30 22:51:26 +0300

    added greeting of salesmans

--- middlewares/init.js
+++ middlewares/init.js
@@ -1,5 +1,10 @@
 
 module.exports = function(req, res, next) {
-    res.locals.user = req.session.user || {user:{isLogined: false}};
+    let type = req.query.who;
+
+    let user = (type == "Salesman")? {type: "Salesman", isLogined: false}: { isLogined: false} ;
+
+    res.locals.user = req.session.user || user;
+
     next();
 }
